2e2cdfd48570775e79c15125ff245c20efe4bf9d
Rui Wang <gilbertwr2012@gmail.com>
2019-03-22 14:35:54 -0400

    Finish the experiment. Start drafting the paper

--- multi-goal Greedy executing and replanning/statistics_draw.py
+++ multi-goal Greedy executing and replanning/statistics_draw.py
@@ -9,17 +9,26 @@ plt.switch_backend('TKagg')
 
 if __name__ == '__main__':
 	
-	file1_nObs_g = "./statistics_ExecutionReplanning/nObstacles/nObstacles_performance.txt";
-	file2_nPosesPerObs_g = "./statistics_ExecutionReplanning/nPosesPerObs/nPosesPerObs_performance.txt";
-	file3_distrVar_g = "./statistics_ExecutionReplanning/distrVar/distrVar_performance.txt";
+	file1_nObs = "./statistics_ExecutionReplanning/nObstacles/nObstacles_performance.txt";
+	file2_nPosesPerObs = "./statistics_ExecutionReplanning/nPosesPerObs/nPosesPerObs_performance.txt";
+	file3_distrVar = "./statistics_ExecutionReplanning/distrVar/distrVar_performance.txt";
 
 	## write in my first file
 	############################################################################
-	f = open(file1_nObs_g)
+	f = open(file1_nObs)
 	nObs = []
 	time_g = []
 	nReplan_g = []
 	pathLength_g = []
+	pathUtilityRate_g = []
+	time_a = []
+	nReplan_a = []
+	pathLength_a = []
+	pathUtilityRate_a = []
+	time_m = []
+	nReplan_m = []
+	pathLength_m = []
+	pathUtilityRate_m = []	
 
 	_counter = 0;
 
@@ -31,41 +40,77 @@ if __name__ == '__main__':
 		time_g.append(line[1])
 		nReplan_g.append(line[2])
 		pathLength_g.append(line[3])
+		pathUtilityRate_g.append(line[4])
+		time_a.append(line[5])
+		nReplan_a.append(line[6])
+		pathLength_a.append(line[7])
+		pathUtilityRate_a.append(line[8])
+		time_m.append(line[9])
+		nReplan_m.append(line[10])
+		pathLength_m.append(line[11])
+		pathUtilityRate_m.append(line[12])
+
+	# IPython.embed()
 
 	## Let's plot the figures
 	plt.figure(1)
-	plt.plot(nObs, time_g, 'bs--', label='Greedy Search')
+	plt.plot(nObs, time_g, 'rs--', label='MaxSucess Greedy')
+	plt.plot(nObs, time_a, 'y^--', label="A*")
+	plt.plot(nObs, time_m, 'bo--', label="MaxSurvival")
 	plt.legend(loc='upper left')
 	plt.xlabel("#obstacles")
-	plt.xlim((3, 55))
-	plt.ylabel("Computation time(s)")
-	plt.ylim((0, 0.03))
+	plt.xlim((8, 65))
+	plt.ylabel("computation time(s)")
+	plt.ylim((0, 1))
 
 	plt.figure(2)
-	plt.plot(nObs, nReplan_g, 'bs--', label='Greedy Search') 
+	plt.plot(nObs, nReplan_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nObs, nReplan_a, 'y^--', label="A*")
+	plt.plot(nObs, nReplan_m, 'bo--', label="MaxSurvival") 
 	plt.legend(loc='upper left')
 	plt.xlabel("#obstacles")
-	plt.xlim((3, 55))
+	plt.xlim((8, 65))
 	plt.ylabel("#replans")
-	plt.ylim((0, 2))
+	plt.ylim((0, 5))
 
 	plt.figure(3)
-	plt.plot(nObs, pathLength_g, 'bs--', label='Greedy Search') 
+	plt.plot(nObs, pathLength_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nObs, pathLength_a, 'y^--', label="A*")
+	plt.plot(nObs, pathLength_m, 'bo--', label="MaxSurvival")  
 	plt.legend(loc='upper left')
 	plt.xlabel("#obstacles")
-	plt.xlim((3, 55))
+	plt.xlim((8, 65))
 	plt.ylabel("path length")
-	plt.ylim((40,70))
+	plt.ylim((0,80))
+
+	plt.figure(4)
+	plt.plot(nObs, pathUtilityRate_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nObs, pathUtilityRate_a, 'y^--', label="A*")
+	plt.plot(nObs, pathUtilityRate_m, 'bo--', label="MaxSurvival")  
+	plt.legend(loc='lower right')
+	plt.xlabel("#obstacles")
+	plt.xlim((8, 65))
+	plt.ylabel("path utility rate")
+	plt.ylim((0.5, 1.2))
 	##############################################################################
 
 
 	## write in my second file
 	############################################################################
-	f = open(file2_nPosesPerObs_g)
+	f = open(file2_nPosesPerObs)
 	nPosesPerObs = []
 	time_g = []
 	nReplan_g = []
 	pathLength_g = []
+	pathUtilityRate_g = []
+	time_a = []
+	nReplan_a = []
+	pathLength_a = []
+	pathUtilityRate_a = []
+	time_m = []
+	nReplan_m = []
+	pathLength_m = []
+	pathUtilityRate_m = []
 
 	_counter = 0;
 
@@ -77,40 +122,74 @@ if __name__ == '__main__':
 		time_g.append(line[1])
 		nReplan_g.append(line[2])
 		pathLength_g.append(line[3])
+		pathUtilityRate_g.append(line[4])
+		time_a.append(line[5])
+		nReplan_a.append(line[6])
+		pathLength_a.append(line[7])
+		pathUtilityRate_a.append(line[8])
+		time_m.append(line[9])
+		nReplan_m.append(line[10])
+		pathLength_m.append(line[11])
+		pathUtilityRate_m.append(line[12])
 
 	## Let's plot the figures
-	plt.figure(4)
-	plt.plot(nPosesPerObs, time_g, 'bs--', label='Greedy Search')
-	plt.legend(loc='upper left')
-	plt.xlabel("#Poses per obstacle")
-	plt.xlim((1, 10))
-	plt.ylabel("Computation time(s)")
-	plt.ylim((0, 0.1))
-
 	plt.figure(5)
-	plt.plot(nPosesPerObs, nReplan_g, 'bs--', label='Greedy Search') 
+	plt.plot(nPosesPerObs, time_g, 'rs--', label='MaxSucess Greedy')
+	plt.plot(nPosesPerObs, time_a, 'y^--', label="A*")
+	plt.plot(nPosesPerObs, time_m, 'bo--', label="MaxSurvival")
 	plt.legend(loc='upper left')
-	plt.xlabel("#Poses per obstacle")
+	plt.xlabel("#poses per obstacle")
 	plt.xlim((1, 10))
-	plt.ylabel("#replans")
-	plt.ylim((0, 3))
+	plt.ylabel("computation time(s)")
+	plt.ylim((0, 1))
 
 	plt.figure(6)
-	plt.plot(nPosesPerObs, pathLength_g, 'bs--', label='Greedy Search') 
+	plt.plot(nPosesPerObs, nReplan_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nPosesPerObs, nReplan_a, 'y^--', label="A*")
+	plt.plot(nPosesPerObs, nReplan_m, 'bo--', label="MaxSurvival") 
 	plt.legend(loc='upper left')
-	plt.xlabel("#Poses per obstacle")
+	plt.xlabel("#poses per obstacle")
+	plt.xlim((1, 10))
+	plt.ylabel("#replans")
+	plt.ylim((0, 5))
+
+	plt.figure(7)
+	plt.plot(nPosesPerObs, pathLength_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nPosesPerObs, pathLength_a, 'y^--', label="A*")
+	plt.plot(nPosesPerObs, pathLength_m, 'bo--', label="MaxSurvival")  
+	plt.legend(loc='upper left')
+	plt.xlabel("#poses per obstacle")
 	plt.xlim((1, 10))
 	plt.ylabel("path length")
-	plt.ylim((40,70))
+	plt.ylim((0,80))
+
+	plt.figure(8)
+	plt.plot(nPosesPerObs, pathUtilityRate_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(nPosesPerObs, pathUtilityRate_a, 'y^--', label="A*")
+	plt.plot(nPosesPerObs, pathUtilityRate_m, 'bo--', label="MaxSurvival")  
+	plt.legend(loc='lower right')
+	plt.xlabel("#poses per obstacle")
+	plt.xlim((1, 10))
+	plt.ylabel("path utility rate")
+	plt.ylim((0.5, 1.2))
 	##############################################################################
 
 	## write in my third file
 	############################################################################
-	f = open(file3_distrVar_g)
+	f = open(file3_distrVar)
 	distrVar = []
 	time_g = []
 	nReplan_g = []
 	pathLength_g = []
+	pathUtilityRate_g = []
+	time_a = []
+	nReplan_a = []
+	pathLength_a = []
+	pathUtilityRate_a = []
+	time_m = []
+	nReplan_m = []
+	pathLength_m = []
+	pathUtilityRate_m = []
 
 	_counter = 0;
 
@@ -122,31 +201,56 @@ if __name__ == '__main__':
 		time_g.append(line[1])
 		nReplan_g.append(line[2])
 		pathLength_g.append(line[3])
+		pathUtilityRate_g.append(line[4])
+		time_a.append(line[5])
+		nReplan_a.append(line[6])
+		pathLength_a.append(line[7])
+		pathUtilityRate_a.append(line[8])
+		time_m.append(line[9])
+		nReplan_m.append(line[10])
+		pathLength_m.append(line[11])
+		pathUtilityRate_m.append(line[12])
 
 	## Let's plot the figures
-	plt.figure(7)
-	plt.plot(distrVar, time_g, 'bs--', label='Greedy Search')
-	plt.legend(loc='upper left')
-	plt.xlabel("#obstacle distribution variance")
-	plt.xlim((0, 6))
-	plt.ylabel("Computation time(s)")
-	plt.ylim((0, 0.1))
-
-	plt.figure(8)
-	plt.plot(distrVar, nReplan_g, 'bs--', label='Greedy Search') 
-	plt.legend(loc='upper left')
-	plt.xlabel("#obstacle distribution variance")
-	plt.xlim((0, 6))
-	plt.ylabel("#replans")
-	plt.ylim((0, 3))
-
 	plt.figure(9)
-	plt.plot(distrVar, pathLength_g, 'bs--', label='Greedy Search') 
+	plt.plot(distrVar, time_g, 'rs--', label='MaxSucess Greedy')
+	plt.plot(distrVar, time_a, 'y^--', label="A*")
+	plt.plot(distrVar, time_m, 'bo--', label="MaxSurvival")
 	plt.legend(loc='upper left')
-	plt.xlabel("#obstacle distribution variance")
-	plt.xlim((0, 6))
+	plt.xlabel("Variance of obstacle distribution")
+	plt.xlim((0.0, 5.0))
+	plt.ylabel("computation time(s)")
+	plt.ylim((0, 1))
+
+	plt.figure(10)
+	plt.plot(distrVar, nReplan_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(distrVar, nReplan_a, 'y^--', label="A*")
+	plt.plot(distrVar, nReplan_m, 'bo--', label="MaxSurvival") 
+	plt.legend(loc='upper left')
+	plt.xlabel("Variance of obstacle distribution")
+	plt.xlim((0.0, 5.0))
+	plt.ylabel("#replans")
+	plt.ylim((0, 5))
+
+	plt.figure(11)
+	plt.plot(distrVar, pathLength_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(distrVar, pathLength_a, 'y^--', label="A*")
+	plt.plot(distrVar, pathLength_m, 'bo--', label="MaxSurvival")  
+	plt.legend(loc='upper left')
+	plt.xlabel("Variance of obstacle distribution")
+	plt.xlim((0.0, 6.0))
 	plt.ylabel("path length")
-	plt.ylim((40,70))
+	plt.ylim((0,80))
+
+	plt.figure(12)
+	plt.plot(distrVar, pathUtilityRate_g, 'rs--', label='MaxSuccess Greedy')
+	plt.plot(distrVar, pathUtilityRate_a, 'y^--', label="A*")
+	plt.plot(distrVar, pathUtilityRate_m, 'bo--', label="MaxSurvival")  
+	plt.legend(loc='lower right')
+	plt.xlabel("Variance of obstacle distribution")
+	plt.xlim((0.0, 5.0))
+	plt.ylabel("path utility rate")
+	plt.ylim((0.5, 1.2))
 	##############################################################################
 
 
